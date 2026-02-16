8ddb51bdffc6d436bdcbf1305d5ac0062b84b1bd
Amin Bajelan <m.amin.bajelan@gmail.com>
2026-02-17 00:20:13 +0330

    create admin panel for model user

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import User
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display=["id", "email", "name", "is_active", "is_staff"]
+    list_filter=["email", "is_active", "is_staff"]
+    search_fields=["email", "name", "is_active", "is_staff", "date_joined"]
+    
--- core/models.py
+++ core/models.py
@@ -1,13 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager    
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.utils import timezone
 from uuid import uuid4
 
-# Custom User Manager
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
-            raise ValueError('Email must be provided')
+            raise ValueError("Email must be provided")
+
+        if not password:
+            raise ValueError("Users must have a password")
+
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -17,21 +21,41 @@ class UserManager(BaseUserManager):
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
         return self.create_user(email, password, **extra_fields)
 
-# Custom User model
 
 class User(AbstractBaseUser, PermissionsMixin):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    email = models.EmailField(unique=True, max_length=255)  
-    name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False,verbose_name="user id:")
+    email = models.EmailField(unique=True, max_length=255,null=False, blank=False,verbose_name="emial address:")
+    name = models.CharField(max_length=255, verbose_name="full name:")
+    is_active = models.BooleanField(default=True,verbose_name="is active:")
+    is_staff = models.BooleanField(default=False,verbose_name="is_staff")
+    date_joined = models.DateTimeField(default=timezone.now,verbose_name="created time:")
+    # if you use deafult+timezone.now it can be override after create
+    # else if you use auto_now_add=True it can't override after create
+    updated_time = models.DateTimeField(auto_now=True,verbose_name="updated time:")
+
     objects = UserManager()
-    
+
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name'] 
-    
+    REQUIRED_FIELDS = ['name'] # don't forget this field is require for superuser :)
+
+    def save(self, *args, **kwargs):
+        self.email = self.__class__.objects.normalize_email(self.email)
+        super().save(*args, **kwargs)
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
     def __str__(self):
-        return self.email
+        return f"{self.name} ({self.email})"
